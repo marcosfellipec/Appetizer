a2956af8a2f3b1b3c832b0bdd739a37c1e77f2ef
Marcos-F Costa <Marcos-F.Costa@btgpactual.com>
2023-12-29 19:59:26 -0300

    Initial Commit

---
+++ Appetizer/AppetizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizerApp.swift
+//  Appetizer
+//
+//  Created by Marcos-F Costa on 29/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
